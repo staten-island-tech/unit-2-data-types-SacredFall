e5fce66d9e3695cc020362cf60f9e2904b5e0b2c
Eric Chen <ericc404@nycstudents.net>
2024-02-02 14:00:45 -0500

    Create skibidirizzpart2.py

---
+++ skibidirizzpart2.py
@@ -0,0 +1,12 @@
+import math 
+from math import *
+
+print("First Number:")
+x = input()
+
+print("Second Number:")
+y = input()
+sum = float(x) + float(y)
+print("Your sum is:", sum)
+
+print(math.exp(4))
