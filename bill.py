f72e7b9840aa427a60a0f857eabbbdf2bb9249f8
Fallods <ericc404@nycstudents.net>
2024-02-05 16:59:43 -0500

    Update bill.py

--- bill.py
+++ bill.py
@@ -10,21 +10,52 @@ def tipcalc():
     global x
     x = (percent*subtotal/100)
     print(subtotal + int(x)) """
-name = input('What is your name?')
+def isfloat(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+
+
+def subtotal():
+    subtotal = input("> $")
+    if isfloat(subtotal):
+        subtotal = float(subtotal)
+    else:
+        print('Restart please, remember do not be an idiot and put a number this time!')
+
+
+print('What is your name?')
+name = input("> ")
+print(f'How much was your subtotal {name}? (In dollars please)')
+subtotal()
+
+
+
 
 def tiporgay():
-    subtotal = float(input(f'How much was your subtotal {name}?'))
+
+    subtotal = input("> $")
+    if isfloat(subtotal):
+        subtotal = float(subtotal)
+    else:
+        print('Restart please, remember do not be an idiot and put a number this time!')
+        tiporgay()
+
     print('How was our service?')
     print('Please respond with bad, okay, good, or great')
-    ourservice = input(": ")
+    ourservice = input("> ")
     if(ourservice == "bad"):
         print("We're sorry, you do not need to tip, please tell us what we could have done better in Skibidi Restaurant")
-    elif(ourservice == okay):
-        print('We reccommend a tip of ' + ourservice*0.15)
-    elif(ourservice == good):
-        print('We reccommend a tip of ' + ourservice*0.20)
-    elif(ourservice == great):
-        print('We reccommend a tip of ' + ourservice*0.25)
+    elif(ourservice == "okay"):
+        print(f'We reccommend a tip of ${subtotal*float(0.15)}')
+    elif(ourservice == "good"):
+        print(f'We reccommend a tip of ${subtotal*float(0.20)}')
+    elif(ourservice == "great"):
+        print(f'We reccommend a tip of ${subtotal*float(0.25)}')
+    else:
+        print('incorrect input, please try again')
 
 tiporgay()
 
