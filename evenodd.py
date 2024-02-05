5ab422c37637c78c1ff21bc0521b92886f3ad253
Eric Chen <ericc404@nycstudents.net>
2024-02-05 14:00:52 -0500

    Create evenodd.py

---
+++ evenodd.py
@@ -0,0 +1,14 @@
+#Let's create a function that determines if a number is odd or even
+
+
+input('SKIBIDI TOILET EVEN CALC')
+print("What is the number you want to check?")
+
+
+usernumber = int(input("> "))
+
+if(usernumber % 2 == 0):
+    print('Your number is even!')
+
+else:
+    print('IT IS ODD EWWWWWWWWWWWWWWWWWWWWWWWWWWWW go back to skibidi world')
