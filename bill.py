f50c5c25cd490af24ae4e512eb2380ffb912ca5a
Fallods <ericc404@nycstudents.net>
2024-02-05 17:12:18 -0500

    Update bill.py

--- bill.py
+++ bill.py
@@ -1,15 +1,4 @@
 #Let's create a function to accept a "bill" value and offer a tip of 0%, 15%, 20% or 25% depending on if the service was "bad, okay, good , or great ". 
- 
-
-""" print('What is your subtotal?')
-subtotal = float(input())
-print ('What is your tip percent?')
-percent = float(input())
-
-def tipcalc():
-    global x
-    x = (percent*subtotal/100)
-    print(subtotal + int(x)) """
 def isfloat(num):
     try:
         float(num)
@@ -18,30 +7,18 @@ def isfloat(num):
         return False
 
 
-def subtotal():
+def subtotal1():
+    global subtotal
     subtotal = input("> $")
     if isfloat(subtotal):
         subtotal = float(subtotal)
+        tiporgay()
+    
     else:
         print('Restart please, remember do not be an idiot and put a number this time!')
-
-
-print('What is your name?')
-name = input("> ")
-print(f'How much was your subtotal {name}? (In dollars please)')
-subtotal()
-
-
-
+    subtotal1()
 
 def tiporgay():
-
-    subtotal = input("> $")
-    if isfloat(subtotal):
-        subtotal = float(subtotal)
-    else:
-        print('Restart please, remember do not be an idiot and put a number this time!')
-        tiporgay()
 
     print('How was our service?')
     print('Please respond with bad, okay, good, or great')
@@ -56,8 +33,19 @@ def tiporgay():
         print(f'We reccommend a tip of ${subtotal*float(0.25)}')
     else:
         print('incorrect input, please try again')
+        tiporgay()
 
-tiporgay()
+
+print('What is your name?')
+name = input("> ")
+print(f'How much was your subtotal {name}? (In dollars please)')
+subtotal1()
+
+
+
+
+
+
 
 
         
