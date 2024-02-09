0cca940c9187e38bf79b4c934af37a54d15bda35
Eric Chen <ericc404@nycstudents.net>
2024-02-09 13:35:24 -0500

    Create word counter.py

---
+++ word counter.py
@@ -0,0 +1,9 @@
+# Challenge
+#1. Using the "input" method in Python, ask a user to input a sentence. Then develop a function that accepts a the user input and will tell you how many words are in that string. First write out your plan in Pseudo-code using comments. Then craft the function. 
+
+
+yoursentence = input('What is your sentence: ')
+
+x = yoursentence.split()
+
+print(len(x))
